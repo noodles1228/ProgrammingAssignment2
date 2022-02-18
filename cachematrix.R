31e76eb172a95b9e07267974841e4f69a624c95e
noodles1228 <99001511+noodles1228@users.noreply.github.com>
2022-02-18 17:05:18 -0600

    Programming assignment 2 final copy

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+# in short, these functions main purpose is to return the inverse of an invertible matric
 
-## Write a short comment describing this function
+#creates get and set functions which will allow us to run cacheinverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#gets the cache data
+cacheinverse <- function(x, ...) {
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("cache data")
+                return(i)
+        }
+        mat_data <- x$get()
+        i <- solve(mat_data, ...)
+        x$setinverse(i)
+        i
 }
+
+
+
+
+
+
